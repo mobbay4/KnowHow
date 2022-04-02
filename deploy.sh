0e056d68770d897daca20850f76323a4ebbb4866
Florian Schweiger <flo.schweiger@campus.lmu.de>
2022-04-02 12:52:15 +0200

    Update documentation

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd ../master/docs/
 make html
-cd ../gh-pages
-rm -rf ./docs/*
+cd ../../gh-pages
+ls
+#rm -rf ./docs/*
 cp -r ../master/docs/_build/html/* ./docs
 git add .
 git commit -am "Update documentation"
